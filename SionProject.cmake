9d29894d91e6a6d9d7029fc89be0c2028122fba2
Nuri Park <nuripark1256@korea.ac.kr>
2016-07-22 02:43:24 +0900

    Add ImProc Dependency

--- SionProject.cmake
+++ SionProject.cmake
@@ -1,3 +1,4 @@
 include("${CMAKE_SOURCE_DIR}/external/Tensor/SionProject.cmake")
+include("${CMAKE_SOURCE_DIR}/external/Tensor-ImProc/SionProject.cmake")
 
 include_directories("${CMAKE_CURRENT_LIST_DIR}/include")
